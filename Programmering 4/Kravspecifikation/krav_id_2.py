8e5b105f5600eba16afda20aa7394483086fb269
Your Name <magnushej@gmail.com>
2023-10-03 12:11:39 +0200

    saved

---
+++ Programmering 4/Kravspecifikation/krav_id_2.py
@@ -0,0 +1,49 @@
+from neopixel import NeoPixel #Importerer de libraries vi behøver
+# initialiserer ADC objekt på på pin 34
+from machine import Pin
+from time import sleep_ms,sleep
+led1 = Pin(26, Pin.OUT, value=0) # Laver instans af Pin objekt til at styre ledl
+n = 13 # Mængder pixel i ring
+p = 26 #Pin i NeoPixel
+np = NeoPixel(Pin(p, Pin.OUT), n) #Skabe NeoPixel instanse
+tal1 = 0
+tal2 = 1
+pb1=Pin(4, Pin.IN)
+
+def clear(): # Definere en funktion at slukke alle lamper
+    for i in range(n):
+        np[i] = (0, 0 , 0)
+        np.write()
+        
+def pxhop(r, g , b): # definition at tænde lamper 0-12
+    for i in range(tal1,tal2):
+        np[i] = (r, g , b)
+        np.write()
+        sleep_ms(400)
+        
+def pxhopminus(r,g,b):
+    for i in range(tal3,tal4):
+        np[i] = (r, g , b)
+        tal3 = tal1 - 1
+        tal4 = tal2 - 1
+        np.write()
+        sleep_ms(40)
+        
+while True: # starter uendeligt while loop
+        if pb1.value() == 0:
+            led1.value(not led1.value())
+            pxhop(100,0,0)
+            if tal2 < 13:
+                tal1 = tal1 + 1
+                tal3 = tal1 - 1
+                tal4 = tal2 - 1
+                tal2 = tal2 + 1
+                print(tal1,tal2)
+                print(tal3,tal4)
+            if tal2 == 13:
+                tal1 = 11
+                tal2 = 12
+                sleep_ms(5)
+                tal1 = 0
+                tal2 = 1
+                print(tal1,tal2)

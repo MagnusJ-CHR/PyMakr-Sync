80aa8770dab933b30657bef9502be4374f73da12
Your Name <magnushej@gmail.com>
2023-10-04 12:24:57 +0200

    thonny

--- Programmering 4/Kravspecifikation/krav_id_4klient.py
+++ Programmering 4/Kravspecifikation/krav_id_4klient.py
@@ -3,7 +3,7 @@ from socket import AF_INET
 from socket import SOCK_DGRAM
 from sys import exit
 
-server_name = input("IP address esp32")
+server_name = input("Ip address her")
 server_port = 12000
 client_socket = socket(AF_INET, SOCK_DGRAM)
 
@@ -13,6 +13,6 @@ while True:
         client_socket.sendto(message.encode(), (server_name, server_port))
         modified_message, server_address = client_socket.recvfrom(2048)
         print(modified_message.decode())
-    except KeyboardInterrupt
-    client_socket.close()
-    exit()
+    except KeyboardInterrupt:
+        client_socket.close()
+        exit()
